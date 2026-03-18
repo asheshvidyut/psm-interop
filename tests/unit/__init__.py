fb6746544306d3872c82b836555f5f79fcb43eef
Ashesh Vidyut <asheshvidyut@google.com>
2026-03-18 19:07:04 +0530

    add env variable

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+
+os.environ["CLOUDSDK_CONTEXT_AWARE_USE_CLIENT_CERTIFICATE"] = "false"
